4162d2d1f3f9c4d41a0c74754cc65134eda3cb0d
Targoran <p.sterker@gmail.com>
2015-11-19 19:22:50 +0100

    Version Bump (1.0.1)

--- autoid.php
+++ autoid.php
@@ -3,7 +3,7 @@
  *
  * AutoID Plugin for Kirby 2
  *
- * @version   1.0
+ * @version   1.0.1
  * @author    Helllicht medien GmbH <http://helllicht.com>
  * @copyright Helllicht medien GmbH <http://helllicht.com>
  * @link      https://github.com/helllicht/kirby-autoid
